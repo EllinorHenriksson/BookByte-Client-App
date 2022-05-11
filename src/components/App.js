a6f5bd6d1c00ae30813df12dd16373022d35a3ca
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-05-11 15:24:55 +0200

    Initialize project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+/**
+ * The App component.
+ *
+ * @returns {string} A string.
+ */
+function App () {
+  return (
+    <div className="app">
+      <h1>BookByte</h1>
+    </div>
+  )
+}
+
+export default App
