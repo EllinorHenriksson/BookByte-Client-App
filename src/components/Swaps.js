b7c1c41a91b885e666c085764d6c70655cd7dec7
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-06-02 11:39:37 +0200

    Add header and explanatory text to Swaps page

--- src/components/Swaps.js
+++ src/components/Swaps.js
@@ -96,42 +96,46 @@ function Swaps (props) {
 
   return (
     <div className="swaps">
-      { book && <BookInfo book={ book } setBook={ setBook }/> }
-      { swapper && <SwapperInfo swapper={ swapper } setSwapper={ setSwapper }/> }
-      { isLoading && <p>Loading...</p> }
-      { swaps?.length === 0 && <p>No swaps at the moment.</p> }
-      { swaps?.length > 0 &&
-        <div className='swap-list'>
-          { swaps.map((swap, i) => (
-            <div className="swap-item" key={ i } id={ i }>
-              <div className="swap-book">
-                <div>
-                  <img src={swap.toGet.imageLinks.smallThumbnail} alt="Book cover" />
-                  <p>GET</p>
+      <h2>Swaps</h2>
+      <p>This page shows a list of possible swaps to make with another user: what book to get, what book to give and what user to swap with. To suggest a swap, simply send the other user an email!</p>
+      <div className="swaps-content">
+        { book && <BookInfo book={ book } setBook={ setBook }/> }
+        { swapper && <SwapperInfo swapper={ swapper } setSwapper={ setSwapper }/> }
+        { isLoading && <p>Loading...</p> }
+        { swaps?.length === 0 && <p>No swaps at the moment.</p> }
+        { swaps?.length > 0 &&
+          <div className='swap-list'>
+            { swaps.map((swap, i) => (
+              <div className="swap-item" key={ i } id={ i }>
+                <div className="swap-book">
+                  <div>
+                    <img src={swap.toGet.imageLinks.smallThumbnail} alt="Book cover" />
+                    <p>GET</p>
+                  </div>
+                  <h3>{ swap.toGet.title }</h3>
+                  { swap.toGet.subtitle && <h4>{ swap.toGet.subtitle }</h4> }
+                  <p>Authors: { swap.toGet.authors.join(', ') }</p>
+                  <button className="to-get" onClick={ handleClickBook }></button>
                 </div>
-                <h3>{ swap.toGet.title }</h3>
-                { swap.toGet.subtitle && <h4>{ swap.toGet.subtitle }</h4> }
-                <p>Authors: { swap.toGet.authors.join(', ') }</p>
-                <button className="to-get" onClick={ handleClickBook }></button>
-              </div>
-              <div className="swap-book">
-                <div>
-                  <img src={swap.toGive.imageLinks.smallThumbnail} alt="Book cover" />
-                  <p>GIVE</p>
+                <div className="swap-book">
+                  <div>
+                    <img src={swap.toGive.imageLinks.smallThumbnail} alt="Book cover" />
+                    <p>GIVE</p>
+                  </div>
+                  <h3>{ swap.toGive.title }</h3>
+                  { swap.toGive.subtitle && <h4>{ swap.toGive.subtitle }</h4> }
+                  <p>Authors: { swap.toGive.authors.join(', ') }</p>
+                  <button className='to-give' onClick={ handleClickBook }></button>
                 </div>
-                <h3>{ swap.toGive.title }</h3>
-                { swap.toGive.subtitle && <h4>{ swap.toGive.subtitle }</h4> }
-                <p>Authors: { swap.toGive.authors.join(', ') }</p>
-                <button className='to-give' onClick={ handleClickBook }></button>
+                <div className="swapper">
+                  <img alt="Profile" src="images/profile.png" />
+                  <p>Swapper: <b>{ swap.otherUser.username }</b></p>
+                  <button onClick={ handleClickSwapper }>Contact</button>
+                </div>
               </div>
-              <div className="swapper">
-                <img alt="Profile" src="images/profile.png" />
-                <p>Swapper: <b>{ swap.otherUser.username }</b></p>
-                <button onClick={ handleClickSwapper }>Contact</button>
-              </div>
-            </div>
-          )) }
-        </div> }
+            )) }
+          </div> }
+      </div>
     </div>
   )
 }
