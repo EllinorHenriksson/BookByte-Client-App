cb8d5856b7c8b4228aa4fc02ccbb7e89f691668e
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-06-17 11:55:03 +0200

    Add Privacy Policy

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -16,7 +16,11 @@ function PrivacyPolicy (props) {
         <h2>Privacy Policy</h2>
       </header>
       <main>
-        <p>Privacy policy</p>
+        <h3>Data use</h3>
+        <p>Data about registered users and their books is stored in the website’s own databases and used to match books and present swap suggestions with contact details to the other user participating in the swap. Data is also needed to authenticate the user.</p>
+        <h3>Delete data</h3>
+        <p>Authenticated users can delete their account by opening the Profile page, clicking on the Delete button and confirming that they want to delete their account by clicking on a second Delete button. All personal data will then be deleted. </p>
+        <a href="pdf/privacy-policy.pdf"><img alt='download' src="images/download.png" />Privacy Policy</a>
       </main>
     </div>
   )
